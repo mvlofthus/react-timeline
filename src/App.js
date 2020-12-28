7609f83f646128f14b95ff4bb2a1bae87acaeb7d
Mackenzie <mvlofthus@gmail.com>
2020-12-27 16:41:13 -0800

    struggling through lessons, things show up now, but apparently i didn't commit anything previously out of sheer confusion.  statuses all show

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
 
+import TimelineEvent from './components/TimelineEvent';
+import Timestamp from './components/Timestamp'
+
 function App() {
   console.log(timelineData);
 
   // Customize the code below
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">{timelineData.person}'s social media feed</h1>
       </header>
       <main className="App-main">
+        <Timeline events={timelineData.events}/>
       </main>
     </div>
   );
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,19 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
+const Timeline = (props) => {
+  const posts = props.events.map((post) => {
+    return (
+      <TimelineEvent key={post.timeStamp} person={post.person} status={post.status} time={post.timeStamp}/>
+    )
+  })
   
-  return;
+
+  return (
+    <div className="timeline">
+      {posts}
+    </div>
+  );
 }
 
 export default Timeline;
